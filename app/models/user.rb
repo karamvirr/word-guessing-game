4abbd60699bcd5d87cc198bb54c129b322a6d806
Karamvir Rai <krai@cloudsecurityalliance.org>
2022-11-23 19:09:43 -0800

    rspec + factorybot setup

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,10 @@
 class User < ApplicationRecord
   belongs_to :room, optional: false
+  before_validation :initialize_slug
+
+private
+  def initialize_slug
+    return if self.slug.present?
+    self.slug = self.name.parameterize
+  end
 end
--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,10 @@ module WordGuessingGame
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
 
+    # This will cause FactoryBot to automatically load factories in
+    # custom/factories.rb and custom/factories/*.rb
+    # config.factory_bot.definition_file_paths = ["custom/factories"]
+
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
