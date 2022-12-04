186e84119daf8214cf2674cd4bd9096fa92570a4
Karamvir Rai <krai@cloudsecurityalliance.org>
2022-12-03 18:41:55 -0800

    redirect 404's to root

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,9 @@ Rails.application.routes.draw do
   get "/staging_area/:slug", to: "landing_pages#staging_area", as: "staging_area"
   get "/create_room", to: "landing_pages#create_room", as: "create_room"
 
+  # Redirect all 404's to root path
+  get "*path" => redirect("/")
+
   # Serve websocket cable requests in-process
   mount ActionCable.server => "/cable"
 end
